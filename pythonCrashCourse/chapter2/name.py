9226f969ecd521bde012e784b880d89971218c34
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-10-30 23:40:11 -0300

    concatenates 2 strings into one variable

--- pythonCrashCourse/chapter2/name.py
+++ pythonCrashCourse/chapter2/name.py
@@ -3,3 +3,8 @@ print(name.title())
 
 print('all uppercase', name.upper())
 print('all lowercase,', name.lower())
+
+first_name = 'lucas'
+last_name = "barrozo"
+full_name = first_name +' '+ last_name
+print(full_name)
