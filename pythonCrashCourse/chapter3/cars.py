4c88364cbd0da26c8ddb55d4b6b6f3c854dedef1
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-01 17:51:11 -0300

    uses sorted(list) to sort a list without changing the original

--- pythonCrashCourse/chapter3/cars.py
+++ pythonCrashCourse/chapter3/cars.py
@@ -1,5 +1,14 @@
 cars = ['honda', 'mitsubish', 'audi', 'fiat']
 
+print('Here is the original list:', cars)
+
+print('\n here is the sorted list:', sorted(cars))
+
+print('\n here is the original list again:', cars)
+
 cars.sort()
 
+print(cars)
+
+cars.sort(reverse=True)
 print(cars)
