a3972c87af4922ae828aad33bac41753b2abaa52
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-02 11:19:50 -0300

    Try it yourself 1 and 2

---
+++ pythonCrashCourse/chapter4/TY_4.1.py
@@ -0,0 +1,6 @@
+pizzas = ['calabresa', 'peperoni', 'frago com catupiry']
+
+for pizza in pizzas:
+    print('Probably my favorite pizza is',pizza)
+
+print('I kinda enjoy pizza')
---
+++ pythonCrashCourse/chapter4/TY_4.2.py
@@ -0,0 +1,6 @@
+animals = ['dog', 'cat', 'turtle', 'dragon']
+
+for animal in animals:
+    print('A ' +animal+' would make a great pet')
+
+print('All pets are slaves')
