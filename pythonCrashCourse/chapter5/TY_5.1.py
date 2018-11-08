8f4755094401fbe3cb967dbcffe5eb7d7537c492
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-08 00:43:56 -0200

    try it yourself 5.1-5.3

---
+++ pythonCrashCourse/chapter5/TY_5.1.py
@@ -0,0 +1,7 @@
+cars = 'honda'
+
+print('Is car == "subaru"? I dont think so')
+print(cars == 'subaru')
+
+print('\n is car =="honda"? I predict true')
+print(cars == 'honda')
---
+++ pythonCrashCourse/chapter5/TY_5.3.py
@@ -0,0 +1,7 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('+5 points')
+
+if alien_color == 'red':
+    print('+15 points')
