3435af63d0102ca9c3c5e7d9f2a9e25b2d143688
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-08 01:32:51 -0200

    using lists in lists

--- pythonCrashCourse/chapter5/requested_items.py
+++ pythonCrashCourse/chapter5/requested_items.py
@@ -6,3 +6,18 @@ if requested_items:
     print('\n finished making your pizza')
 else:
     print('are you sure you want a plain pizza ?')
+
+
+##using multiple lists
+
+avaiable_toppings = ['calabresa', 'queijo', 'pepperoni']
+
+requested_toppings = ['calabresa', 'queijo', 'frango']
+
+for requested_topping in requested_toppings:
+    if requested_topping in avaiable_toppings:
+        print('adding ', requested_topping)
+    else:
+        print('sorry, topping not available')
+
+print('finished making your pizza')
