f056eb2bd5a027be5514b92f1705b25dc73f8f15
Lucas Lange Barrozo <lucaslbarrozo@gmail.com>
2018-11-08 01:00:34 -0200

    using if statement to check if list is empty

---
+++ pythonCrashCourse/chapter5/requested_items.py
@@ -0,0 +1,8 @@
+requested_items =['calabresa', 'milho' ]
+
+if requested_items:
+    for requested in requested_items:
+        print('Adding '+ requested +'.')
+    print('\n finished making your pizza')
+else:
+    print('are you sure you want a plain pizza ?')
