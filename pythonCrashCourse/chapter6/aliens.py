c5cadd70f2e2ec088dccece6e6289e94f230f159
Lucas <lucaslbarrozo@gmail.com>
2018-11-12 20:22:11 -0200

    adds new key value pairs to the dictionary

--- pythonCrashCourse/chapter6/aliens.py
+++ pythonCrashCourse/chapter6/aliens.py
@@ -3,3 +3,11 @@ alien_0 = {'color':'green', 'points':5}
 print(alien_0['color'])
 
 print(alien_0['points'])
+
+print(alien_0)
+
+alien_0['x_position']= 0
+
+alien_0['y_position'] = 25
+
+print(alien_0)
