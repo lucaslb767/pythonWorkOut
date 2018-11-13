a4f9087113496dba2f2c5433b3e309ac1d276aab
Lucas <lucaslbarrozo@gmail.com>
2018-11-13 02:47:13 -0200

    uses del statement to remove a value from dictionary

--- pythonCrashCourse/chapter6/aliens.py
+++ pythonCrashCourse/chapter6/aliens.py
@@ -11,3 +11,9 @@ alien_0['x_position']= 0
 alien_0['y_position'] = 25
 
 print(alien_0)
+
+#to delete a piece of information you dont need anymore use the del statement
+
+del alien_0['points']
+
+print(alien_0)
