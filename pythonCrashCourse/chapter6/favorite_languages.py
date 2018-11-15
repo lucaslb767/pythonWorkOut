98f65de1efc5385fe6051537064a3cf6dfd9e90d
Lucas <lucaslbarrozo@gmail.com>
2018-11-15 18:05:51 -0200

    uses the value method to loop only through values

--- pythonCrashCourse/chapter6/favorite_languages.py
+++ pythonCrashCourse/chapter6/favorite_languages.py
@@ -5,3 +5,20 @@ favorite_languages = {
 }
 
 print('Jon favorite language is ', favorite_languages['jon'])
+
+friends = ['sarah']
+
+#using a list to sort a dictionary's value
+for name in favorite_languages:
+    print(name.title())
+
+    if name in friends:
+        print('Hi', name.title(),'I see your favorite language is', favorite_languages[name],'!')
+
+
+#using the value() method to only loop trhough values
+
+print('the following values have been mentioned:')
+
+for language in favorite_languages.values():
+    print(language.title())
