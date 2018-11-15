ff28b6865bab75fa450f0666260c9a764b6f781c
Lucas <lucaslbarrozo@gmail.com>
2018-11-15 18:19:15 -0200

    do it yourself 6.5

---
+++ pythonCrashCourse/chapter6/TY_6.5.py
@@ -0,0 +1,8 @@
+rivers ={
+    'nile':'egypt',
+    'amazonas': 'Brazil',
+    'Mississipi': 'USA'
+}
+
+for river, country in rivers.items():
+    print('The', river.title(),'runs trhough', country.title() )
